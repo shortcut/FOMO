52136f4a2fb5dd4118bf8466abfa818a373759eb
Vikram <vikram.sh86@gmail.com>
2025-02-21 16:23:53 +0100

    Initial Commit 2

---
+++ FOMO/FOMO/FOMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOMOApp.swift
+//  FOMO
+//
+//  Created by Vikram on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FOMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
