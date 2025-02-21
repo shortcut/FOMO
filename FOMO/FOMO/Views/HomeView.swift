dedd2106418cec50abf31ae640518e287c50aa78
Vikram <vikram.sh86@gmail.com>
2025-02-21 17:59:12 +0100

    Some start of design

--- FOMO/FOMO/Views/HomeView.swift
+++ FOMO/FOMO/Views/HomeView.swift
@@ -5,15 +5,43 @@ struct HomeView: View {
     @State private var articles: [Article] = []
     var body: some View {
         List(articles, id: \.id) { article in
-            Text(article.title)
-        }
-        .onAppear {
-            Task {
-                let articles = await self.service.fetchArticles()
-                withAnimation(.bouncy) {
-                    self.articles = articles
+            VStack(spacing: 8) {
+                if let url = URL(string: article.image ?? "") {
+                    AsyncImage(url: url) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: 150)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                )
+
+                        default:
+                            EmptyView()
+                        }
+                    }
                 }
+
+                Text(article.title)
+                    .font(.system(.title3, design: .monospaced, weight: .semibold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
             }
+            .frame(maxWidth: .infinity)
+        }
+        .task {
+            await performSearchArticles("Elon musk")
+        }
+    }
+}
+
+private extension HomeView {
+    func performSearchArticles(_ keyword: String = "") async {
+        let articles = await self.service.fetchArticles()
+        withAnimation(.bouncy) {
+            self.articles = articles
         }
     }
 }
