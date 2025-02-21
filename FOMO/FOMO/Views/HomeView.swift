5692765b376ed3d01ca9a93e4ed579e920dfda64
Vikram <vikram.sh86@gmail.com>
2025-02-21 20:03:06 +0100

    Ragnar went Berzerk

--- FOMO/FOMO/Views/HomeView.swift
+++ FOMO/FOMO/Views/HomeView.swift
@@ -18,107 +18,132 @@ struct HomeView: View {
 
     var body: some View {
         NavigationStack {
+//            Picker("", selection: $segmentetThemes) {
+//                ForEach(SegmentetThemes.allCases, id: \.self) { theme in
+//                    Text(theme.rawValue)
+//                }
+//            }
+//            .pickerStyle(SegmentedPickerStyle())
+//            .padding(.horizontal)
+
             List(articles) { article in
                 Button {
 
                 } label: {
-                    VStack(spacing: 8) {
-                        if let url = URL(string: article.image ?? "") {
-                            HStack {
-                                AsyncImage(url: url) { phase in
-                                    switch phase {
-                                    case .success(let image):
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(maxWidth: .infinity)
-                                            .clipShape(
-                                                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                                            )
-                                            .overlay(alignment: .topTrailing) {
-                                                Text("Breaking news")
-                                                    .font(.caption)
-                                                    .padding(4)
-                                                    .background(.red.gradient)
-                                                    .foregroundStyle(.white)
-                                                    .clipShape(Capsule())
-                                                    .rotationEffect(.degrees(27.5))
-                                                    .offset(x: 10, y: 10)
-                                            }
-                                        
-                                    default:
-                                        EmptyView()
-                                    }
-                                }
-                            }
-                            .background(
-                                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                                    .fill(Color.blue.gradient.opacity(0.2))
-                            )
-                        }
-
-                        Text(article.title)
-                            .font(.system(.title3, design: .monospaced, weight: .semibold))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-
-                        Text(article.body)
-                            .lineLimit(3)
-                            .foregroundStyle(.secondary)
-
-                        HStack {
-                            Text(article.dateTimePub, style: .relative)
-                                .animation(.smooth)
-
-                            Text(article.source.title)
-
-                            Spacer()
-                            Button {
-
-                            } label: {
-                                Image(systemName: "bookmark")
-                            }
-
-                        }
-                        .font(.caption)
-                        .bold()
-                        .foregroundStyle(.tertiary)
-                        .padding(.vertical)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 8, style: .continuous)
-                            .stroke(style: .init(lineWidth: 0.5))
-                            .foregroundStyle(.separator)
-                    )
+                    sumeST(article)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                .stroke(style: .init(lineWidth: 0.5))
+                                .foregroundStyle(.separator)
+                        )
                 }
                 .listRowSeparator(.hidden)
                 .listRowBackground(Color.clear)
             }
             .buttonStyle(ListButtonStyle())
             .listStyle(.plain)
-            .toolbar(content: {
-                ToolbarItemGroup(placement: .principal) {
-                    Picker("", selection: $segmentetThemes) {
-                        ForEach(SegmentetThemes.allCases, id: \.self) { theme in
-                            Text(theme.rawValue)
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-            })
+//            .toolbar(content: {
+//                ToolbarItemGroup(placement: .principal) {
+//                    Picker("", selection: $segmentetThemes) {
+//                        ForEach(SegmentetThemes.allCases, id: \.self) { theme in
+//                            Text(theme.rawValue)
+//                        }
+//                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+//                }
+//            })
             .searchable(text: $observer.searchText, prompt: "Search news...")
+            .searchScopes($segmentetThemes, activation: .onSearchPresentation) {
+                ForEach(SegmentetThemes.allCases, id: \.self) { theme in
+                    Text(theme.rawValue).tag(theme)
+                }
+            }
             .task {
                 await performSearchArticles()
             }
             .navigationTitle("Articles")
+            .onChange(of: segmentetThemes) { _ , newValue in
+                Task {
+                    observer.searchText = newValue.rawValue
+                   // await performSearchArticles(newValue.rawValue)
+                }
+            }
             .onChange(of: observer.debouncedText) { _ , newValue in
                 Task {
                     await performSearchArticles(newValue)
                 }
             }
         }
+    }
+
+    @ViewBuilder
+    func sumeST(_ article: Article) -> some View {
+        VStack(spacing: 8) {
+            if let url = URL(string: article.image ?? "") {
+                HStack {
+                    AsyncImage(url: url) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                )
+                                .overlay(alignment: .topTrailing) {
+                                    Text("Breaking news")
+                                        .font(.caption)
+                                        .padding(4)
+                                        .background(.red.gradient)
+                                        .foregroundStyle(.white)
+                                        .clipShape(Capsule())
+                                        .rotationEffect(.degrees(27.5))
+                                        .offset(x: 10, y: 10)
+                                }
+
+                        default:
+                            EmptyView()
+                        }
+                    }
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .fill(Color.blue.gradient.opacity(0.2))
+                )
+            }
+
+            Text(article.title)
+                .font(.system(.title3, design: .monospaced, weight: .semibold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Text(article.body)
+                .lineLimit(3)
+                .foregroundStyle(.secondary)
+
+            HStack {
+                Text(article.dateTimePub, style: .relative)
+                    .animation(.smooth)
+
+                Text(article.source.title)
+
+                Spacer()
+                Button {
+
+                } label: {
+                    Image(systemName: "bookmark")
+                }
+
+            }
+            .font(.caption)
+            .bold()
+            .foregroundStyle(.tertiary)
+            .padding(.vertical)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+
     }
 }
 
