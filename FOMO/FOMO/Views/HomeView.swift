6b06dbf739e2c88709c97318e5646add58c3656b
Kjetil Terjesen <kjetil@shortcut.no>
2025-02-21 18:56:13 +0100

    add segmented controller

--- FOMO/FOMO/Views/HomeView.swift
+++ FOMO/FOMO/Views/HomeView.swift
@@ -1,38 +1,60 @@
 import SwiftUI
 
+enum SegmentetThemes: String, CaseIterable {
+    case all = "All"
+    case tech = "Tech"
+    case business = "Business"
+    case sports = "Sports"
+    case world = "World"
+}
+
 struct HomeView: View {
     let service = ArticleAPIService()
     @State private var articles: [Article] = []
-    var body: some View {
-        List(articles, id: \.id) { article in
-            VStack(spacing: 8) {
-                if let url = URL(string: article.image ?? "") {
-                    AsyncImage(url: url) { phase in
-                        switch phase {
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(height: 150)
-                                .clipShape(
-                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
-                                )
+    @State var segmentetThemes: SegmentetThemes = .all
 
-                        default:
-                            EmptyView()
+    var body: some View {
+        NavigationStack {
+            List(articles, id: \.id) { article in
+                VStack(spacing: 8) {
+                    if let url = URL(string: article.image ?? "") {
+                        AsyncImage(url: url) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(height: 150)
+                                    .clipShape(
+                                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                    )
+
+                            default:
+                                EmptyView()
+                            }
                         }
                     }
+
+                    Text(article.title)
+                        .font(.system(.title3, design: .monospaced, weight: .semibold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
                 }
-
-                Text(article.title)
-                    .font(.system(.title3, design: .monospaced, weight: .semibold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
-        }
-        .task {
-            await performSearchArticles("Elon musk")
+            .toolbar(content: {
+                ToolbarItemGroup(placement: .principal) {
+                    Picker("", selection: $segmentetThemes) {
+                        ForEach(SegmentetThemes.allCases, id: \.self) { theme in
+                            Text(theme.rawValue)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            })
+            .task {
+                await performSearchArticles("Elon musk")
+            }
         }
     }
 }
